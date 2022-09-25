5577ac8946cf89ee695df902929a7b9f774860a1
Osadchyi Maksym <114179499+normskinew@users.noreply.github.com>
2022-09-25 13:08:46 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello W2423423423432424434324orld!");
         }
     }
 }

ed82b0b380aa2661b955139b7409c227d6334b8b
max <normskinew@gmail.com>
2022-10-02 12:54:58 +0300

    123

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -17,59 +17,65 @@ namespace ConsoleApp2
             answer = Console.ReadLine();
             if (answer == "y")
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Point A({x},{y}) has been generated!");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("According to the Pythagorean theorem, the sum of the squares of the legs is equal to the square of the hypotenuse!\nx2 + y2 = c2;");
-                c = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine($"Hypotenuse= {c}");
-                Console.ForegroundColor = ConsoleColor.White;
-                if (c <= radiusMass[1] && c >= radiusMass[0])
+                string ans;
+                do
                 {
-                    Console.WriteLine("You have 10 points");
-                }
-                else if (c <= radiusMass[2] && c > radiusMass[1])
-                {
-                    Console.WriteLine("You have 9 points");
-                }
-                else if (c <= radiusMass[3] && c > radiusMass[2])
-                {
-                    Console.WriteLine("You have 8 points");
-                }
-                else if (c <= radiusMass[4] && c > radiusMass[3])
-                {
-                    Console.WriteLine("You have 7 points");
-                }
-                else if (c <= radiusMass[5] && c > radiusMass[4])
-                {
-                    Console.WriteLine("You have 6 points");
-                }
-                else if (c <= radiusMass[6] && c > radiusMass[5])
-                {
-                    Console.WriteLine("You have 5 points");
-                }
-                else if (c <= radiusMass[7] && c > radiusMass[6])
-                {
-                    Console.WriteLine("You have 4 points");
-                }
-                else if (c <= radiusMass[8] && c > radiusMass[7])
-                {
-                    Console.WriteLine("You have 3 points");
-                }
-                else if (c <= radiusMass[9] && c > radiusMass[8])
-                {
-                    Console.WriteLine("You have 2 points");
-                }
-                else if (c <= radiusMass[10] && c > radiusMass[9])
-                {
-                    Console.WriteLine("You have 1 points");
-                }
-                else 
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You have 0 points");
-                }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Point A({x},{y}) has been generated!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("According to the Pythagorean theorem, the sum of the squares of the legs is equal to the square of the hypotenuse!\nx2 + y2 = c2;");
+                    c = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"Hypotenuse= {c}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (c <= radiusMass[1] && c >= radiusMass[0])
+                    {
+                        Console.WriteLine("You have 10 points");
+                    }
+                    else if (c <= radiusMass[2] && c > radiusMass[1])
+                    {
+                        Console.WriteLine("You have 9 points");
+                    }
+                    else if (c <= radiusMass[3] && c > radiusMass[2])
+                    {
+                        Console.WriteLine("You have 8 points");
+                    }
+                    else if (c <= radiusMass[4] && c > radiusMass[3])
+                    {
+                        Console.WriteLine("You have 7 points");
+                    }
+                    else if (c <= radiusMass[5] && c > radiusMass[4])
+                    {
+                        Console.WriteLine("You have 6 points");
+                    }
+                    else if (c <= radiusMass[6] && c > radiusMass[5])
+                    {
+                        Console.WriteLine("You have 5 points");
+                    }
+                    else if (c <= radiusMass[7] && c > radiusMass[6])
+                    {
+                        Console.WriteLine("You have 4 points");
+                    }
+                    else if (c <= radiusMass[8] && c > radiusMass[7])
+                    {
+                        Console.WriteLine("You have 3 points");
+                    }
+                    else if (c <= radiusMass[9] && c > radiusMass[8])
+                    {
+                        Console.WriteLine("You have 2 points");
+                    }
+                    else if (c <= radiusMass[10] && c > radiusMass[9])
+                    {
+                        Console.WriteLine("You have 1 points");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You have 0 points");
+                    }
+                    Console.WriteLine("Press (y/n) to continue");
+
+                } while (ans == "y");
 
 
 

75469cba628807038d9c736f15d8dc914a96de88
max <normskinew@gmail.com>
2022-10-10 23:07:10 +0300

    Home_work_6_task_1

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Home_Work_6_task_1";
+        Console.ForegroundColor = ConsoleColor.Green;
+        int[] salary = new int[5];
+        string[] surname = new string[] { "Oleg", "Igor", "Maria", "Tanya", "Max" };
+        Random random = new Random();
+        for (int i = 0; i < surname.Length; i++)
+        {
+            Console.WriteLine(surname[i] + ": "+random.Next(6500,100000));
+        }
+
+        Console.ReadKey();
+
+    }
+}

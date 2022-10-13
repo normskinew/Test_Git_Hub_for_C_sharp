417e5cb3feda2be2a458fe8f30e0f2ef60d4f026
max <normskinew@gmail.com>
2022-10-13 19:02:46 +0300

    Home_Work_6_task_1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,18 +1,20 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Title = "Home_Work_6_task_1";
+        //Home_Work_6_task_1
+
+        Console.Title = "Work";
         Console.ForegroundColor = ConsoleColor.Green;
         int[] salary = new int[5];
         string[] surname = new string[] { "Oleg", "Igor", "Maria", "Tanya", "Max" };
         Random random = new Random();
         for (int i = 0; i < surname.Length; i++)
         {
-            Console.WriteLine(surname[i] + ": "+random.Next(6500,100000));
+            Console.WriteLine(surname[i] + ": " + random.Next(6500, 100000));
         }
-
-        Console.ReadKey();
-
+                
     }
 }

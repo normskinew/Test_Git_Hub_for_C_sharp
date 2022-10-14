4836e5b7eec695d26844ef440a2442d0fb3612bd
max <normskinew@gmail.com>
2022-10-14 16:49:59 +0300

    fix

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
 
 internal class Program
 {
@@ -7,14 +9,25 @@ internal class Program
         //Home_Work_6_task_1
 
         Console.Title = "Work";
-        Console.ForegroundColor = ConsoleColor.Green;
-        int[] salary = new int[5];
-        string[] surname = new string[] { "Oleg", "Igor", "Maria", "Tanya", "Max" };
         Random random = new Random();
+        int[] salary = new int[5];
+        for (int i = 0; i < salary.Length; i++)
+        {
+            
+            salary[i] = random.Next(6500, 100000);
+            
+        }
+        
+        string[] surname = new string[] { "Oleg", "Igor", "Maria", "Tanya", "Max" };
         for (int i = 0; i < surname.Length; i++)
         {
-            Console.WriteLine(surname[i] + ": " + random.Next(6500, 100000));
+            
+            Console.WriteLine($"The salary of {surname[i]} is {salary[i]}");
         }
-                
+
+
+
+        Console.ReadKey();
+
     }
 }
